bf55ff4c089faac22be04e51fc2d7a698507ea62
hqwang09 <hqwang09@gmail.com>
2015-09-14 21:47:32 -0400

    initial check in

---
+++ src/polls/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+from _csv import list_dialects
+
+# Register your models here.
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+#    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
